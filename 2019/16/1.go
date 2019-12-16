8fdd71409ea040f1a1aa2b6067aa4629c34c9677
mnml <pyr0spr1t3@gmail.com>
2019-12-16 05:17:42 -0500

    2019 day 16

---
+++ 2019/16/1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	signal := strings.TrimSpace(string(input))
+
+	for p := 0; p < 100; p++ {
+		output := ""
+		for i := range signal {
+			sum := 0
+			for j, c := range signal {
+				sum += int(c-'0') * []int{0, 1, 0, -1}[(j+1)/(i+1)%4]
+			}
+			if sum < 0 {
+				sum = -sum
+			}
+			output += string('0' + sum%10)
+		}
+		signal = output
+	}
+
+	fmt.Println(signal[:8])
+}
