102ce0c1c7ea9cfd27d46d2a54ca271dab5ec398
mnml <pyr0spr1t3@gmail.com>
2020-12-03 03:43:00 -0500

    simplify 2020 day 3

--- 2020/03/2.go
+++ 2020/03/2.go
@@ -10,31 +10,17 @@ import (
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 
-	grid := map[image.Point]rune{}
-	w, h := 0, 0
-	for y, s := range strings.Fields(string(input)) {
-		for x, r := range s {
-			grid[image.Point{x, y}] = r
-		}
-		w, h = len(s), y+1
-	}
-
-	slopes := []image.Point{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
-	coords, ntrees := make([]image.Point, len(slopes)), map[image.Point]int{}
-	for y := 0; y < h; y++ {
-		for i, s := range slopes {
-			if grid[coords[i]] == '#' {
-				ntrees[s]++
-			}
-			coords[i] = coords[i].Add(s)
-			coords[i].X %= w
-		}
-	}
-
+	grid := strings.Fields(string(input))
+	slopes := map[image.Point]int{{1, 1}: 0, {3, 1}: 0, {5, 1}: 0, {7, 1}: 0, {1, 2}: 0}
 	mult := 1
-	for _, v := range ntrees {
-		mult *= v
+	for s := range slopes {
+		for p := (image.Point{}); p.Y < len(grid); p = p.Add(s) {
+			if grid[p.Y][p.X%len(grid[0])] == '#' {
+				slopes[s]++
+			}
+		}
+		mult *= slopes[s]
 	}
-	fmt.Println(ntrees[image.Point{3, 1}])
+	fmt.Println(slopes[image.Point{3, 1}])
 	fmt.Println(mult)
 }
