f6d5af64a5bf0ee15f652b73bec4d8c2c92aeeef
mnml <pyr0spr1t3@gmail.com>
2020-12-04 07:24:54 -0500

    combine 2020 day 4

--- 2020/04/2.go
+++ 2020/04/2.go
@@ -8,27 +8,30 @@ import (
 )
 
 var res = []*regexp.Regexp{
-	regexp.MustCompile(`byr:(19[2-9]\d|200[0-2])(\s+|$)`),
-	regexp.MustCompile(`iyr:(201\d|2020)(\s+|$)`),
-	regexp.MustCompile(`eyr:(202\d|2030)(\s+|$)`),
-	regexp.MustCompile(`hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)(\s+|$)`),
-	regexp.MustCompile(`hcl:#[\da-f]{6}(\s+|$)`),
-	regexp.MustCompile(`ecl:(amb|blu|brn|gry|grn|hzl|oth)(\s+|$)`),
-	regexp.MustCompile(`pid:\d{9}(\s+|$)`),
+	regexp.MustCompile(`(?:^|\s)(byr):(?:(19[2-9]\d|200[0-2])(?:\s|$))?`),
+	regexp.MustCompile(`(?:^|\s)(iyr):(?:(201\d|2020)(?:\s|$))?`),
+	regexp.MustCompile(`(?:^|\s)(eyr):(?:(202\d|2030)(?:\s|$))?`),
+	regexp.MustCompile(`(?:^|\s)(hgt):(?:((?:1[5-8]\d|19[0-3])cm|(?:59|6\d|7[0-6])in)(?:\s|$))?`),
+	regexp.MustCompile(`(?:^|\s)(hcl):(?:(#[\da-f]{6})(?:\s|$))?`),
+	regexp.MustCompile(`(?:^|\s)(ecl):(?:(amb|blu|brn|gry|grn|hzl|oth)(?:\s|$))?`),
+	regexp.MustCompile(`(?:^|\s)(pid):(?:(\d{9})(?:\s|$))?`),
 }
 
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 
-	valid := 0
-out:
+	part1, part2 := 0, 0
 	for _, s := range strings.Split(strings.TrimSpace(string(input)), "\n\n") {
+		d1, d2 := 1, 1
 		for _, re := range res {
-			if !re.MatchString(s) {
-				continue out
+			if m := re.FindStringSubmatch(s); len(m) == 0 {
+				d1, d2 = 0, 0
+			} else if m[2] == "" {
+				d2 = 0
 			}
 		}
-		valid++
+		part1, part2 = part1+d1, part2+d2
 	}
-	fmt.Println(valid)
+	fmt.Println(part1)
+	fmt.Println(part2)
 }
