029d53cc34c753d4d590a082f8a9e88a40668145
mnml <pyr0spr1t3@gmail.com>
2020-12-06 16:49:14 -0500

    simplify 2018 day 11

--- 2018/11/2.go
+++ 2018/11/2.go
@@ -8,31 +8,25 @@ func main() {
 	var cells [size + 1][size + 1]int
 	for y := 1; y <= size; y++ {
 		for x := 1; x <= size; x++ {
-			cells[x][y] = ((x+10)*y+serial)*(x+10)/100%10 - 5 + cells[x][y-1] + cells[x-1][y] - cells[x-1][y-1]
+			cells[y][x] = ((x+10)*y+serial)*(x+10)/100%10 - 5 + cells[y-1][x] + cells[y][x-1] - cells[y-1][x-1]
 		}
 	}
 
-	var max, mx, my, ms int
-	for x := 1; x <= size-2; x++ {
-		for y := 1; y <= size-2; y++ {
-			sum := cells[x+2][y+2] + cells[x-1][y-1] - cells[x+2][y-1] - cells[x-1][y+2]
-			if sum > max {
-				max, mx, my = sum, x, y
-			}
-		}
-	}
-	fmt.Printf("%d,%d\n", mx, my)
-
-	max = 0
-	for s := 1; s <= size; s++ {
-		for x := 1; x <= size-s+1; x++ {
-			for y := 1; y <= size-s+1; y++ {
-				sum := cells[x+s-1][y+s-1] + cells[x-1][y-1] - cells[x+s-1][y-1] - cells[x-1][y+s-1]
-				if sum > max {
-					max, mx, my, ms = sum, x, y, s
+	var p1m, p1x, p1y int
+	var p2m, p2x, p2y, p2s int
+	for y := 1; y <= size; y++ {
+		for x := 1; x <= size; x++ {
+			for s := 1; x+s <= size && y+s <= size; s++ {
+				sum := cells[y+s-1][x+s-1] + cells[y-1][x-1] - cells[y-1][x+s-1] - cells[y+s-1][x-1]
+				if s == 3 && sum > p1m {
+					p1m, p1x, p1y = sum, x, y
+				}
+				if sum > p2m {
+					p2m, p2x, p2y, p2s = sum, x, y, s
 				}
 			}
 		}
 	}
-	fmt.Printf("%d,%d,%d\n", mx, my, ms)
+	fmt.Printf("%d,%d\n", p1x, p1y)
+	fmt.Printf("%d,%d,%d\n", p2x, p2y, p2s)
 }
