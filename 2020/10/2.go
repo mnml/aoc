08682c5f3263c39667c10a5876b6c33efdcd3702
mnml <pyr0spr1t3@gmail.com>
2020-12-10 02:47:06 -0500

    simplify 2020 day 10

--- 2020/10/2.go
+++ 2020/10/2.go
@@ -19,15 +19,11 @@ func main() {
 	sort.Ints(jolts)
 	jolts = append([]int{0}, append(jolts, jolts[len(jolts)-1]+3)...)
 
-	diff := map[int]int{}
-	for i := 1; i < len(jolts); i++ {
-		diff[jolts[i]-jolts[i-1]]++
-	}
-	fmt.Println(diff[1] * diff[3])
-
-	memo := map[int]int{0: 1}
-	for _, v := range jolts[1:] {
+	diff, memo := map[int]int{}, map[int]int{0: 1}
+	for i, v := range jolts[1:] {
+		diff[v-jolts[i]]++
 		memo[v] = memo[v-1] + memo[v-2] + memo[v-3]
 	}
+	fmt.Println(diff[1] * diff[3])
 	fmt.Println(memo[jolts[len(jolts)-1]])
 }
