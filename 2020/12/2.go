ea76c531c640f618ad80f5141aded5d3a787a218
mnml <pyr0spr1t3@gmail.com>
2020-12-12 06:36:20 -0500

    refactor 2020 day 12

--- 2020/12/2.go
+++ 2020/12/2.go
@@ -4,44 +4,38 @@ import (
 	"fmt"
 	"image"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
+	ins := strings.Split(strings.TrimSpace(string(input)), "\n")
 
-	ship1, dir := image.Point{0, 0}, 1
-	ship2, wp := image.Point{0, 0}, image.Point{10, -1}
-	for _, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+	ship, wp := image.Point{0, 0}, image.Point{10, -1}
+	fmt.Println(run(ins, &ship, &image.Point{1, 0}, &ship))
+	fmt.Println(run(ins, &image.Point{0, 0}, &wp, &wp))
+}
+
+func run(ins []string, ship, dir, mov *image.Point) int {
+	delta := map[rune]image.Point{'N': {0, -1}, 'S': {0, 1}, 'E': {1, 0}, 'W': {-1, 0}, 'L': {-1, 1}, 'R': {1, -1}}
+
+	for _, s := range ins {
 		var action rune
 		var value int
 		fmt.Sscanf(s, "%c%d", &action, &value)
 
-		delta := map[rune]image.Point{'N': {0, -1}, 'E': {1, 0}, 'S': {0, 1}, 'W': {-1, 0}}
 		switch action {
 		case 'N', 'S', 'E', 'W':
-			ship1 = ship1.Add(delta[action].Mul(value))
-			wp = wp.Add(delta[action].Mul(value))
-		case 'L':
-			value *= 3
-			fallthrough
-		case 'R':
-			dir = (dir + value/90) % 4
+			*mov = mov.Add(delta[action].Mul(value))
+		case 'L', 'R':
 			for i := 0; i < value/90; i++ {
-				wp.X, wp.Y = -wp.Y, wp.X
+				dir.X, dir.Y = delta[action].Y*dir.Y, delta[action].X*dir.X
 			}
 		case 'F':
-			ship1 = ship1.Add(delta[rune("NESW"[dir])].Mul(value))
-			ship2 = ship2.Add(wp.Mul(value))
+			*ship = ship.Add(dir.Mul(value))
 		}
 	}
-	fmt.Println(abs(ship1.X) + abs(ship1.Y))
-	fmt.Println(abs(ship2.X) + abs(ship2.Y))
-}
 
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+	return int(math.Abs(float64(ship.X)) + math.Abs(float64(ship.Y)))
 }
