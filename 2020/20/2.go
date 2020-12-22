f437f9cec8e060e16c267c46fa550af3f5d86df8
mnml <pyr0spr1t3@gmail.com>
2020-12-22 14:00:52 -0500

    clean 2020 day 20

--- 2020/20/2.go
+++ 2020/20/2.go
@@ -2,153 +2,109 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"io/ioutil"
+	"math"
 	"regexp"
 	"strings"
 )
 
-const isize = 12
-const tsize = 10
+type Tile []string
 
-func main() {
-	input, _ := ioutil.ReadFile("input.txt")
-	split := strings.Split(strings.TrimSpace(string(input)), "\n\n")
-
-	tiles := map[int][]string{}
-	edges := map[string][]int{}
-	for _, s := range split {
-		var id int
-		fmt.Sscanf(s, "Tile %d:", &id)
-		tiles[id] = strings.Split(s, "\n")[1:]
-		for _, e := range getEdges(tiles[id]) {
-			edges[canon(e)] = append(edges[canon(e)], id)
-		}
-	}
-
-	counts := map[int]int{}
-	var corner int
-
-	part1 := 1
-	for _, v := range edges {
-		if len(v) == 1 {
-			if counts[v[0]]++; counts[v[0]] == 2 {
-				corner = v[0]
-				part1 *= corner
-			}
-		}
-	}
-	fmt.Println(part1)
-
-	order := [isize][isize][]string{{tiles[corner]}}
-	for i := 1; len(edges[canon(order[0][0][0])]) != 1 || len(edges[canon(col(order[0][0], 0))]) != 1; i++ {
-		order[0][0] = rotate(tiles[corner], i)
-	}
-	delete(tiles, corner)
-
-	image := []string{}
-	for y := 0; y < isize; y++ {
-		for x := 0; x < isize; x++ {
-			if y == 0 && x == 0 {
-				continue
-			}
-
-			for id, tile := range tiles {
-				if (y == 0 || hasEdge(tile, order[y-1][x][tsize-1])) && (x == 0 || hasEdge(tile, col(order[y][x-1], tsize-1))) {
-					order[y][x] = tile
-					for i := 1; y != 0 && order[y][x][0] != order[y-1][x][tsize-1] || x != 0 && col(order[y][x], 0) != col(order[y][x-1], tsize-1); i++ {
-						order[y][x] = rotate(tile, i)
-					}
-					delete(tiles, id)
-					break
-				}
-			}
-		}
-	}
-
-	part2 := 0
-	for y := 0; y < isize; y++ {
-		for i := 1; i < tsize-1; i++ {
-			image = append(image, "")
-			for x := 0; x < isize; x++ {
-				image[y*(tsize-2)+i-1] += order[y][x][i][1 : tsize-1]
-				part2 += strings.Count(order[y][x][i][1:tsize-1], "#")
-			}
-		}
-	}
-
-	orig := make([]string, len(image))
-	copy(orig, image)
-	for i := 1; i <= 8; i++ {
-		monster := "..................#.\n#....##....##....###\n.#..#..#..#..#..#..."
-		split := strings.Split(monster, "\n")
-
-		for y := 0; y < len(image)-len(split); y++ {
-		search:
-			for x := 0; x < len(image[0])-len(split[0]); x++ {
-				for i, s := range split {
-					if match, _ := regexp.MatchString(s, image[y+i][x:x+len(s)]); !match {
-						continue search
-					}
-				}
-				part2 -= strings.Count(monster, "#")
-			}
-		}
-		image = rotate(orig, i)
-	}
-	fmt.Println(part2)
-}
-
-func getEdges(tile []string) []string {
-	return []string{tile[0], col(tile, len(tile[0])-1), tile[len(tile)-1], col(tile, 0)}
-}
-
-func hasEdge(tile []string, edge string) bool {
-	for _, e := range getEdges(tile) {
-		if canon(e) == canon(edge) {
-			return true
-		}
-	}
-	return false
-}
-
-func reverse(s string) (rs string) {
-	for _, r := range s {
-		rs = string(r) + rs
-	}
-	return
-}
-
-func canon(s string) string {
-	if rs := reverse(s); rs < s {
-		return rs
-	}
-	return s
-}
-
-func col(tile []string, i int) (col string) {
-	for _, s := range tile {
+func (t Tile) Col(i int) (col string) {
+	for _, s := range t {
 		col += string(s[i])
 	}
 	return
 }
 
-func flip(tile []string) (flip []string) {
-	for _, s := range tile {
-		flip = append(flip, reverse(s))
-	}
-	return
+func (t Tile) Edges() []string {
+	return []string{t[0], t.Col(len(t[0]) - 1), t[len(t)-1], t.Col(0)}
 }
 
-func rotate(tile []string, n int) (rot []string) {
-	if n%8 >= 4 {
-		tile = flip(tile)
-	}
-	for ; n > 0; n-- {
-		rot = []string{}
-		for i := range tile {
-			rot = append(rot, reverse(col(tile, i)))
+func (t Tile) Rotations() []Tile {
+	rot := make([]Tile, 8)
+	for r := 0; r < 8; r += 2 {
+		for _, s := range t {
+			rot[r] = append(rot[r], reverse(s))
 		}
-		tile = rot
+		for i := range t {
+			rot[r+1] = append(rot[r+1], reverse(t.Col(i)))
+		}
+		t = rot[r+1]
+	}
+	return rot
+}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	split := strings.Split(strings.TrimSpace(string(input)), "\n\n")
+
+	tiles := map[int]Tile{}
+	counts := map[string]int{}
+	for _, s := range split {
+		var id int
+		fmt.Sscanf(s, "Tile %d:", &id)
+		tiles[id] = strings.Split(s, "\n")[1:]
+		for _, e := range tiles[id].Edges() {
+			counts[e]++
+			counts[reverse(e)]++
+		}
+	}
+
+	imageSize, tileSize := int(math.Sqrt(float64(len(tiles)))), 10
+	img := make(Tile, imageSize*(tileSize-2))
+	order := map[image.Point]Tile{}
+
+	part1 := 1
+	for y := 0; y < imageSize; y++ {
+		for x := 0; x < imageSize; x++ {
+		findTile:
+			for i, t := range tiles {
+				for _, r := range t.Rotations() {
+					if (y == 0 && counts[r[0]] == 1 ||
+						y != 0 && r[0] == order[image.Point{x, y - 1}][tileSize-1]) &&
+						(x == 0 && counts[r.Col(0)] == 1 ||
+							x != 0 && r.Col(0) == order[image.Point{x - 1, y}].Col(tileSize-1)) {
+						if (y == 0 || y == imageSize-1) && (x == 0 || x == imageSize-1) {
+							part1 *= i
+						}
+
+						for i := 0; i < tileSize-2; i++ {
+							img[(tileSize-2)*y+i] += r[i+1][1 : tileSize-1]
+						}
+
+						order[image.Point{x, y}] = r
+						delete(tiles, i)
+						break findTile
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(part1)
+
+	mon := []string{"..................#.", "#....##....##....###", ".#..#..#..#..#..#..."}
+	nmon := 0
+	for _, r := range img.Rotations() {
+		for y := 0; y < len(r)-len(mon); y++ {
+		findMon:
+			for x := 0; x < len(r[0])-len(mon[0]); x++ {
+				for i, s := range mon {
+					if match, _ := regexp.MatchString(s, r[y+i][x:x+len(s)]); !match {
+						continue findMon
+					}
+				}
+				nmon++
+			}
+		}
+	}
+	fmt.Println(strings.Count(strings.Join(img, ""), "#") - nmon*strings.Count(strings.Join(mon, ""), "#"))
+}
+
+func reverse(s string) (rs string) {
+	for _, r := range s {
+		rs = string(r) + rs
 	}
 	return
 }
