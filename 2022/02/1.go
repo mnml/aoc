b3b3e2f2f8809097b48e57dda8206161625d80b2
mnml <pyr0spr1t3@gmail.com>
2022-12-02 14:11:36 -0500

    simplify 2022 day 2

--- 2022/02/1.go
+++ 2022/02/1.go
@@ -9,16 +9,10 @@ import (
 func main() {
 	input, _ := os.ReadFile("input.txt")
 
-	scores := map[string][]int{
-		"A X": {1 + 3, 3 + 0}, "A Y": {2 + 6, 1 + 3}, "A Z": {3 + 0, 2 + 6},
-		"B X": {1 + 0, 1 + 0}, "B Y": {2 + 3, 2 + 3}, "B Z": {3 + 6, 3 + 6},
-		"C X": {1 + 6, 2 + 0}, "C Y": {2 + 0, 3 + 3}, "C Z": {3 + 3, 1 + 6},
-	}
-
 	part1, part2 := 0, 0
 	for _, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
-		part1 += scores[s][0]
-		part2 += scores[s][1]
+		part1 += strings.Index("B XC YA ZA XB YC ZC XA YB Z", s)/3 + 1
+		part2 += strings.Index("B XC XA XA YB YC YC ZA ZB Z", s)/3 + 1
 	}
 	fmt.Println(part1)
 	fmt.Println(part2)
