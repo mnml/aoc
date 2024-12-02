7f3a42586aaf081bc6faeb48b25803bbd18c9e4c
mnml <pyr0spr1t3@gmail.com>
2024-12-02 09:38:26 -0500

    simplify 2024 day 2

--- 2024/02/1.go
+++ 2024/02/1.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"slices"
-	"strconv"
 	"strings"
 )
 
@@ -13,49 +13,28 @@ func main() {
 
 	part1, part2 := 0, 0
 	for _, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
-		report := []int{}
-		for _, s := range strings.Fields(s) {
-			n, _ := strconv.Atoi(s)
-			report = append(report, n)
-		}
+		var report []int
+		json.Unmarshal([]byte("["+strings.ReplaceAll(s, " ", ",")+"]"), &report)
 
 		if check(report) {
 			part1++
 		}
 		for i := range report {
-			if check(append(slices.Clone(report[:i]), report[i+1:]...)) {
+			if check(slices.Delete(slices.Clone(report), i, i+1)) {
 				part2++
 				break
 			}
 		}
 	}
-
 	fmt.Println(part1)
 	fmt.Println(part2)
 }
 
 func check(r []int) bool {
 	for i := 1; i < len(r); i++ {
-		d := r[i] - r[i-1]
-		if sgn(d) != sgn(r[1]-r[0]) || abs(d) < 1 || abs(d) > 3 {
+		if d := r[i] - r[i-1]; d*(r[1]-r[0]) <= 0 || d < -3 || d > 3 {
 			return false
 		}
 	}
 	return true
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func sgn(x int) int {
-	if x < 0 {
-		return -1
-	} else if x > 0 {
-		return 1
-	}
-	return 0
 }
