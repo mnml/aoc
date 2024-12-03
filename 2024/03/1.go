27e4e7358c13b8104bff6965d1aa9e1312e42b25
mnml <pyr0spr1t3@gmail.com>
2024-12-03 03:35:55 -0500

    simplify 2024 day 3

--- 2024/03/1.go
+++ 2024/03/1.go
@@ -5,18 +5,14 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	input, _ := os.ReadFile("input.txt")
 	re := regexp.MustCompile(`(?s)(?:^|do\(\)).*?(?:don't\(\)|$)`)
-
-	part2 := 0
-	for _, m := range re.FindAllString(string(input), -1) {
-		part2 += mul(m)
-	}
 	fmt.Println(mul(string(input)))
-	fmt.Println(part2)
+	fmt.Println(mul(strings.Join(re.FindAllString(string(input), -1), "")))
 }
 
 func mul(s string) (r int) {
