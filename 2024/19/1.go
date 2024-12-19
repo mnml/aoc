879bc6236c1ae347d9d715de969b29440b612da8
mnml <pyr0spr1t3@gmail.com>
2024-12-19 00:27:14 -0500

    2024 day 19

---
+++ 2024/19/1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("input.txt")
+	split := strings.Split(strings.TrimSpace(string(input)), "\n\n")
+
+	var ways func(string) int
+	cache := map[string]int{}
+
+	ways = func(design string) (n int) {
+		if n, ok := cache[design]; ok {
+			return n
+		}
+		defer func() { cache[design] = n }()
+
+		if design == "" {
+			return 1
+		}
+		for _, s := range strings.Split(split[0], ", ") {
+			if strings.HasPrefix(design, s) {
+				n += ways(design[len(s):])
+			}
+		}
+		return n
+	}
+
+	part1, part2 := 0, 0
+	for _, s := range strings.Fields(split[1]) {
+		if w := ways(s); w > 0 {
+			part1++
+			part2 += w
+		}
+	}
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
