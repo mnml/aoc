dcf2c22a88b12bb55acdb6e6ef9034bafa9a216d
mnml <pyr0spr1t3@gmail.com>
2025-12-07 17:33:45 -0500

    simplify 2025 day 7

--- 2025/07/1.go
+++ 2025/07/1.go
@@ -2,47 +2,29 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"os"
 	"strings"
 )
 
 func main() {
 	input, _ := os.ReadFile("input.txt")
+	split := strings.Fields(string(input))
 
-	grid := map[image.Point]rune{}
-	var start image.Point
-	for y, s := range strings.Fields(string(input)) {
-		for x, r := range s {
-			grid[image.Point{x, y}] = r
-			if r == 'S' {
-				start = image.Point{x, y}
+	beams := make([]int, len(split[0]))
+	beams[strings.Index(split[0], "S")] = 1
+
+	part1, part2 := 0, 1
+	for _, s := range split {
+		for i, r := range s {
+			if r == '^' {
+				part1 += min(beams[i], 1)
+				part2 += beams[i]
+				beams[i-1] += beams[i]
+				beams[i+1] += beams[i]
+				beams[i] = 0
 			}
 		}
 	}
-
-	var beam func(image.Point) int
-	cache := map[image.Point]int{}
-	part1 := map[image.Point]struct{}{}
-
-	beam = func(p image.Point) (n int) {
-		if n, ok := cache[p]; ok {
-			return n
-		}
-		defer func() { cache[p] = n }()
-
-		p.Y++
-		if _, ok := grid[p]; !ok {
-			return 1
-		}
-		if grid[p] == '^' {
-			part1[p] = struct{}{}
-			return beam(image.Point{p.X - 1, p.Y}) + beam(image.Point{p.X + 1, p.Y})
-		}
-		return beam(p)
-	}
-
-	part2 := beam(start)
-	fmt.Println(len(part1))
+	fmt.Println(part1)
 	fmt.Println(part2)
 }
